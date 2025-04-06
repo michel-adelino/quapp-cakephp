3e4ff1bef8623c453dc74bc0b577bac7b9f2845d
michel-adelino <michel.adelino.tech@gmail.com>
2025-04-07 00:10:05 +0200

    Update update_team_calcTotal.sql
    fixed sql mode ONLY_FULL_GROUP_BY

--- src/Controller/sql/update_team_calcTotal.sql
+++ src/Controller/sql/update_team_calcTotal.sql
@@ -2,7 +2,7 @@ UPDATE teams t1
     LEFT JOIN (
         SELECT q.*, (@rownum := @rownum + 1) AS rr
         FROM (
-                 SELECT t2.id, count(ty.id) AS cc, sum(ty.endRanking = 1) AS ww, sum(rp.points) AS pp
+                 SELECT max(t2.id) as id, count(ty.id) AS cc, sum(ty.endRanking = 1) AS ww, sum(rp.points) AS pp
                  FROM teams t2
                           LEFT JOIN team_years ty ON (t2.id = ty.team_id AND ty.canceled = 0 AND ty.endRanking > 0 AND
                                                       ty.endRanking IS NOT NULL)
@@ -18,4 +18,4 @@ SET t1.calcTotalYears         = p.cc,
     t1.calcTotalPointsPerYear = FLOOR(100 * (p.pp / p.cc)) / 100,
     t1.calcTotalRanking       = p.rr,
     t1.calcTotalChampionships = p.ww
-WHERE p.cc IS NOT NULL
+WHERE p.cc IS NOT NULL;

4d4f2ec7eb2b32ad0cf3d91780e784de8136a571
michel-adelino <michel.adelino.tech@gmail.com>
2025-04-14 10:27:47 +0200

    Update LoginsController.php
    add changePassword

--- src/Controller/LoginsController.php
+++ src/Controller/LoginsController.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Entity\Login;
+
 /**
  * Logins Controller
  *
@@ -23,5 +25,24 @@ class LoginsController extends AppController
         $this->apiReturn($return);
     }
 
+    public function changePassword(): void
+    {
+        $login = false;
+        $postData = $this->request->getData();
 
+        if (isset($postData['name']) && isset($postData['password']) && isset($postData['newPassword'])
+            && $this->checkUsernamePassword($postData['name'], $postData['password'])) {
+            $login = $this->Logins->find('all')->where(['name' => $postData['name']])->first();
+            /**
+             * @var Login|null $login
+             */
+
+            if ($login) {
+                $login->set('password', md5($postData['newPassword']));
+                $this->Logins->save($login);
+            }
+        }
+
+        $this->apiReturn($login);
+    }
 }
